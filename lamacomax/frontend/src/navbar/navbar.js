c942c1015d9e8ded633ecef584606db55cba1b5b
juliefino <j.fino@students.ephec.be>
2022-03-18 22:04:03 +0100

    Adjusting navbar

--- lamacomax/frontend/src/navbar/navbar.js
+++ lamacomax/frontend/src/navbar/navbar.js
@@ -1,14 +1,67 @@
 import React from 'react';
+import './navbar.css';
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className='container'>
+        <nav className='navbar navbar-expand-lg navbar-mainbg'>
             
-            <h1 className='text-center'>
-                Navbar
-            </h1>
+            <NavLink className='navbar-brand navbar-logo' to='/' exact>
+                Toto
+            </NavLink>
             
-        </div>
+            <button
+                className='navbar-toggler' 
+                type='button' 
+                data-toggle='collapse'
+                data-target='#navbarSupportedContent'
+                aria-controls='navbarSupportedContent'
+                aria-expanded='false'
+                aria-label='Toggle navigation'>
+                <i className='fas fa-bars text-white'></i>
+            </button>
+            
+            <div 
+                className='collapse navbar-collapse'
+                id='navbarSupportContent'>
+                <ul className='navbar-nav ml-auto'>
+                    <div className='hori-selector'>
+                        <div className='left'></div>
+                        <div className='right'></div>
+                    </div>
+                    
+                    <li className='nav-item active'>
+                        <NavLink className='nav-link' to='/' exact>
+                            <i className='fas fa-tachometer-alt'></i>
+                            ACCUEIL
+                        </NavLink>
+                    </li>
+                    
+                    <li className='nav-item active'>
+                        <NavLink className='nav-link' to='/equipe' exact>
+                            <i className='fas fa-tachometer-alt'></i>
+                            EQUIPE
+                        </NavLink>
+                    </li>
+                    
+                    <li className='nav-item active'>
+                        <NavLink className='nav-link' to='/rdv' exact>
+                            <i className='fas fa-tachometer-alt'></i>
+                            RENDEZ-VOUS
+                        </NavLink>
+                    </li>
+                    
+                    <li className='nav-item active'>
+                        <NavLink className='nav-link' to='/contact' exact>
+                            <i className='fas fa-tachometer-alt'></i>
+                            CONTACT
+                        </NavLink>
+                    </li>
+                </ul>
+            </div>
+            
+        </nav>
+            
     )
 }
 
