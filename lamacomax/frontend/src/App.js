421a3b810445a6f4c8834a1c854541293ee53ca4
juliefino <j.fino@students.ephec.be>
2022-04-27 18:14:44 +0200

    Reconfiguration frontend

--- lamacomax/frontend/src/App.js
+++ lamacomax/frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment, useEffect, useState } from "react";
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import './App.css';
 
@@ -10,6 +10,34 @@ import Navbar from "./components/navbar";
 import Espacedoc from "./pages/espacedoc/espacedoc";
 
 const App = () => {
+    
+    /** useEffect(() => {
+        const getAPI = () => {
+            // Change this endpoint to whatever local or online address you have
+            // Local PostgreSQL Database
+            const API = 'http://127.0.0.1:5000/';
+
+            fetch(API)
+                .then((response) => {
+                    console.log(response);
+                    return response.json();
+                })
+                .then((data) => {
+                    console.log(data);
+                    setLoading(false);
+                    setApiData(data);
+                });
+        };
+        getAPI();
+    }, []);
+    
+    const [apiData, setApiData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    return (
+        
+    )
+    **/ 
+    
     return (
         <Router>
             <Navbar />
