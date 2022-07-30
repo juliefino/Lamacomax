4e7ca94ec13f681ba86a43a702d4e338cd9e689a
juliefino <j.fino@students.ephec.be>
2022-07-30 15:19:30 +0200

    Affichage horaires du centre

--- lamacomax/frontend/src/components/formulaireContact.js
+++ lamacomax/frontend/src/components/formulaireContact.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 const FormulaireContact = () => {
+    
+    const [horairescentre, setHorairescentre] = useState([]);
+    
+    const getHorairescentre = async() => {
+        try{
+            const response = await fetch('http://localhost:5000/contact')
+            const data = await response.json()
+            
+            console.log(data);
+            setHorairescentre(data);
+        }
+        catch (err) {
+            console.error(err.message())
+        }
+    };
+    
+    useEffect(() => {
+        getHorairescentre();
+    }, []);
+    
     
     return (
        <div style={{
@@ -24,8 +44,15 @@ const FormulaireContact = () => {
            <div style={{marginTop: '5%'}}>
                <div className="mb-3">
                    <label style={{fontSize: '15px', color: 'white', marginLeft: '13%'}}>Numéro de téléphone : +3271159369</label>
-                    <label style={{fontSize: '15px', color: 'white', marginLeft: '13%'}}>Horaires : </label>
-                     <!-- HORAIRES -->
+                   <label style={{fontSize: '15px', color: 'white', marginLeft: '13%'}}>Horaires : </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Lundi : {horairescentre[0].lundi} </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Mardi : {horairescentre[0].mardi} </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Mercredi : {horairescentre[0].mercredi} </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Jeudi : {horairescentre[0].jeudi} </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Vendredi : {horairescentre[0].vendredi} </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Samedi : {horairescentre[0].samedi} </label>
+                   <label style={{fontSize: '13px', color: 'white', marginLeft: '15%'}}>Dimanche : {horairescentre[0].dimanche} </label>
+                   
                </div>
            </div>
            
