508387b4391b8ea3a34506f1aa7100e25488c67f
juliefino <j.fino@students.ephec.be>
2022-08-01 19:47:51 +0200

    Modifications couleurs page contact + ajout notifications envoi mail

--- lamacomax/frontend/src/components/formulaireContact.js
+++ lamacomax/frontend/src/components/formulaireContact.js
@@ -1,8 +1,8 @@
 import React, {useEffect, useState} from "react";
 import 'react-notifications/lib/notifications.css';
-import {NotificationContainer, NotificationManager} from 'react-notifications';
-
-// NOTIFICATIONS SETUP
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+//import {NotificationContainer, NotificationManager} from 'react-notifications';
 
 const FormulaireContact = () => {
     
@@ -53,25 +53,48 @@ const FormulaireContact = () => {
         let result = await response.json();
         
         if (result.status === "Message Sent") {
-            NotificationManager.success('Message envoyé ! Nous vous recontacterons prochainement !')
+            // NotificationManager.success('Votre message a bien été envoyé, nous vous recontacterons prochainement !')
+            toast.success('Votre message a bien été envoyé, nous vous recontacterons prochainement ! ', {
+                position: "bottom-right",
+                autoClose: 4500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored"
+            });
         } else {
-            NotificationManager.error("Une erreur s'est produite, veuillez réessayer");
+            //NotificationManager.error("Une erreur s'est produite, veuillez réessayer");
+            toast.error('Une erreur s\'est produite, veuillez réessayer', {
+                position: "bottom-right",
+                autoClose: 4500,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: false,
+                progress: undefined,
+                theme: "colored"
+            });
         }
         
         // alert(result.status);
+       
         console.log(typeof (result.status));
+        console.log(result.status);
   };
     
     return (
+        
        <div style={{
            letterSpacing: '5',
            overflow: 'auto',
            height: '50%',
-           width: '50%',
+           width: '45%',
            position: 'relative',
            margin: 'auto',
-           marginTop: '10%',
-           backgroundColor: '#B4D1BD',
+           marginTop: '7%',
+           backgroundColor: '#94afd2',
            borderRadius: "1rem",
            textAlign: "left",
            justifyContent: 'center',
@@ -79,6 +102,7 @@ const FormulaireContact = () => {
            boxShadow: '0px 0px 6px 0px #a5a5a5', 
            padding: '15px'
        }}>
+           
        <form>
            <h2 style={{color: 'white', fontWeight: 'bold', marginTop:'5px', marginLeft:'25px'}}>Contactez-nous</h2> 
            <div style={{marginTop: '5%'}}>
@@ -100,41 +124,56 @@ const FormulaireContact = () => {
       </form>
            <form onSubmit={handleSubmit} >
       <div>
-        <label htmlFor="nom" style={{fontSize: 'calc(1rem + 0.25vw", minWidth:"300px', color: 'white', marginLeft: '13%'}}>Nom:</label>
+        <label htmlFor="nom" style={{fontSize: '1.5rem', color: 'white', marginLeft: '13%'}}>Nom:</label>
         <input type="text" id="nom" required className="form-control"
             placeholder="Entrez votre nom"
             style={{
+                fontSize:'1.3rem',
                 width: '75%',
                 margin: 'auto'
             }}/>
       </div>
       <div>
-        <label htmlFor="email" style={{fontSize: 'calc(1rem + 0.25vw", minWidth:"300px', color: 'white', marginLeft: '13%'}}>Adresse email:</label>
+        <label htmlFor="email" style={{fontSize: '1.5rem', color: 'white', marginLeft: '13%'}}>Adresse email:</label>
         <input type="email" id="email" required  className="form-control"
             placeholder="Entrez votre adresse email"
             style={{
+                fontSize:'1.3rem',
                 width: '75%',
                 margin: 'auto'
             }}/>
       </div>
       <div>
-        <label htmlFor="message" style={{fontSize: 'calc(1rem + 0.25vw", minWidth:"300px', color: 'white', marginLeft: '13%'}}>Message:</label>
+        <label htmlFor="message" style={{fontSize: '1.5rem', color: 'white', marginLeft: '13%'}}>Message:</label>
         <textarea id="message" required  className="form-control"
             placeholder="Ecrivez votre message ..."
             style={{
+                fontSize:'1.3rem',
                 width: '75%',
                 margin: 'auto'
             }}/>
       </div>
       <button type="submit" style={{
-              backgroundColor: '#6C7D71', 
+              backgroundColor: '#5e87bb', 
+              fontSize: '1.5rem',
               width: '40%',
               margin: 'auto',
               color: 'white',
               borderRadius : '1rem'
           }}>{status}</button>
     </form>
-           <NotificationContainer/>
+           <ToastContainer
+                position="bottom-right"
+                theme="colored"
+                autoClose={4500}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable={false}
+                pauseOnHover
+           />
        </div>
     );
 }
